b1c7fb3f198dc82a00c7c0c83a2ac00e9fe4120f
prajaktapatil-18 <new-prajaktapatil0327@gmail.com>
2025-02-28 17:40:28 -0500

    BhagavatGeeta

--- app/src/main/java/com/example/bhagavatgeeta/HomeFragment.kt
+++ app/src/main/java/com/example/bhagavatgeeta/HomeFragment.kt
@@ -1,23 +1,49 @@
 package com.example.bhagavatgeeta
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.bhagavatgeeta.databinding.FragmentHomeBinding
+import com.example.bhagavatgeeta.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
-
-
+    private lateinit var  binding :FragmentHomeBinding
+private val viewModel:MainViewModel by viewModels()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+  getAllChapter()
+    return binding.root
+    }
+
+    private fun getAllChapter() {
+
+lifecycleScope.launch {
+    viewModel.getAllChapter().collect {chapterlist->
+
+        for (i in chapterlist){
+            Log.d("tag",i.toString())
+        }
+
+    }
+}
+
+
     }
 
 
